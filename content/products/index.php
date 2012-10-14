ddb5d8de4f5365b018de66fabab88625b244b4c6
Lasha badashvili <l_badashvili@cu.edu.ge>
2012-10-14 20:27:33 +0400

    Revert "pharmacy"
    This reverts commit 8e1926b1748857b44dc7e2e8995593cee0fe3a48.

--- content/products/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include 'functions.php';
-include 'model.php';
-
-echo embed('tpl/home.product.tpl.php',array('title'=>'products','last_product'=>
-		embed('tpl/last_product.php',array('result'=>last_added())),'body'=>
-			embed('tpl/product.tpl.php',array('result'=>get_product_by_id($_GET['id'])))));
--- content/tpl/search.tpl.php
+++
@@ -1,3 +0,0 @@
-<form action="controller.php" method="get" id ="search">
-	<input id="string" name = "query" type ="search" placeholder="Search..." />
-</form>
