e6ed2ad0b1c652f8ca2751ef0d50584bb24eab44
Lasha badashvili <l_badashvili@cu.edu.ge>
2012-10-28 02:25:20 +0400

    Update tpl/addtocart.tpl.php

--- tpl/addtocart.tpl.php
+++ tpl/addtocart.tpl.php
@@ -1,4 +1,4 @@
-<form class = "form-inline" action = "#" method = "post" id = "addtocart">
+<form class ="form-inline" action ="#" method = "post" id = "addtocart">
 	<input type = "number" name = "quantity" placeholder = "quantity">
 	<input class = "btn btn-success" type = "submit" value = "add to cart">
 </form>
